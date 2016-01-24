ca81a849743dfc602e1c122fc6fc1158b21a63f0
avong886 <alexvong8@gmail.com>
2016-01-23 20:33:02 -0800

    Update retrieval.php

--- retrieval.php
+++ retrieval.php
@@ -1,23 +1,24 @@
 <?php
+
 $servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "myDB";
+$username = "cryszwjw_g_user";
+$password = "vWNxak!auom3";
+$dbname = "cryszwjw_g_stats";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
-     die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "SELECT id, firstname, lastname FROM MyGuests";
+$sql = "SELECT PlayerID, PlayerName, PlayerStats FROM gamesync_data";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
      // output data of each row
      while($row = $result->fetch_assoc()) {
-         echo "<br> id: ". $row["id"]. " - Name: ". $row["firstname"]. " " . $row["lastname"] . "<br>";
+         echo "<br> id: ". $row["PlayerID"]. " - Name: ". $row["PlayerName"]. " - Level: " . $row["PlayerStats"] . "<br>";
      }
 } else {
      echo "0 results";
