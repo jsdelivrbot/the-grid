9080e8b8f6a5a0d6d4fa591652b92b88e7e0e0f3
avong886 <alexvong8@gmail.com>
2016-01-24 13:02:06 -0800

    Create Alternative_fuels.js

---
+++ directory/chem/Alternative_fuels.js
@@ -0,0 +1,35 @@
+var alternative_fuels= modern life was made by discovery and refinery of fossil fuels.  
+Fossil fuels = organic matter.decay();
+Fossil_fuels=[];
+
+Fossil fuels.push(['natural gas']);
+Fossil fuels.push(['gasoline']);
+Fossil fuels.push(['coal, gas, and oil for electrical power']);
+
+//vast reserves of petroleum
+//petroleum: source of liquid hydrocarbon fuels
+//hydrocarbon fuels: gasoline, coal
+
+//this.reserves are limited && irreplaceable 
+
+//alternative and sustainable power generation methods
+
+//hydroelectric power
+//wind power
+//geothermal power
+//solar power
+//alternative fuels
+
+//::purpose:: reduce demand for fossil fuels
+
+//hydrogen, ethanol, methane, biodiesel
+
+//hydrogen extracted from ocean water by electrolysis
+// or...
+// steam reforming of hydrocarbons
+// ch4,g + h20,g --> co,g + 3h2,g 
+//co,g + h20,g --> co2,g + h2,g
+
+// most of hydrogen production relies on hydrocarbons in:
+//petroleum
+//coal
