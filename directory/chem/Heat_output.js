83db8284130f9081a352576671d6a66f5148cda7
avong886 <alexvong8@gmail.com>
2016-01-24 13:21:12 -0800

    Create Heat_output.js

---
+++ directory/chem/Heat_output.js
@@ -0,0 +1,18 @@
+Enthalpy density = enthalpy of combustion per Liter
+::account for burden of mass: specific enthalpy. kJ per gram
+
+
+//Guide:
+
+Fuels already oxygenated have lower enthalpy densities
+
+//Given:
+Enthalpy of combustion( 1 mol)
+Joules of heat needed
+
+Can calculate: 
+Mols needed
+
+Molar mass
+
+Can calculate: mass needed
