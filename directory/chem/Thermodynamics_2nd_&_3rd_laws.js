6f59e1f31bdb9b84c556a531f5259c4998950bca
avong886 <alexvong8@gmail.com>
2016-01-24 14:21:05 -0800

    Update Thermodynamics_2nd_&_3rd_laws.js

--- directory/chem/Thermodynamics_2nd_&_3rd_laws.js
+++ directory/chem/Thermodynamics_2nd_&_3rd_laws.js
@@ -1,1 +1,83 @@
 
+var Spontaneous change= "entropy";
+
+::molecules can occupy only a series of discrete energy levels && possess certain allowed energies
+
+::entropy readout::increase in entropy = more intricate changes accompanying chemical reactions
+
+
+var spontaneousProcess=
+{
+tendency:'true',
+Rapidly:'not necessarily',
+rate:'it depends',
+External_influence:'absent';
+}
+
+::example: gas in a stopper
+::on release: gas fills both chambers spontaneously
+
+::energy and matter tend to disperse in a disorderly fashion
+
+::low entropy: little disorder
+::high entropy: Greta disorder
+
+::entropy of an [isolated] system increase.bind spontaneous change
+
+::cooling of hot metal::increase in entropy
+::isolated system.def = block of hot metal and its surroundings
+
+<>expansion of gas corredponds to increase in entropy
+
+2ND LAW OF THERMODYNAMICS 
+
+Delta_S = q rev /T
+
+q must be transferred reversibly.
+The infinitestessimal of differences causes changes
+
+Context. Temperature 
+T system = constant=t surr.
+
+Heat = J, T = Kelvin.   Delta_S = J/K
+
+Low T causes high change, high increase in entropy, thermal motion. Sneezing in a quiet library attracts attention.
+
+
+Kelvin= 273.15+ 25 = 298 K
+Delta_S = 100.j/ 298 K
+0.336 J/ K
+
+//convert to kelvin. It's not change in energy
+
+
+
+::Heating causes thermal disorder
+Positional disorder: dispersion, mixing
+
+dS = dqrev /T
+dqrev = C.heat capacity *dT
+
+dS = C heat capacity dT/ T
+
+deltaS = integral t1-t2 dS 
+
+deltaS = C heat capacity ln(t1-t2)
+
+Greater the ratio of temp, the greater the entropy. Graphical: delta_S/C increases at an almost infinitesimal rate initially, then rate of change flattens out, vs. T1/T2
+
+
+Thermal disorder
+
+//given: 
+20.0L nitrogen gas, 5.00kPa, from 20 degrees to 400. degrees Celsius, constant volume.
+Delta_S of nitrogen?
+
+Molar heat capacity of Notrogen and constant volume: Cvm: 20 .81 J/K*mol
+
+
+
+
+
+
+
