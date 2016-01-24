b4816a87e71a8647c1848444bee2e671eaea8bc2
avong886 <alexvong8@gmail.com>
2016-01-23 18:41:04 -0800

    Update Start_page.php

--- Start_page.php
+++ Start_page.php
@@ -1,1 +1,25 @@
 
+<?php
+$servername = "localhost";
+$username = "cryszwjw_g_user";
+$password = "vWNxak!auom3";
+$dbname = "cryszwjw_g_stats";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "INSERT INTO  (firstname, lastname, email)
+VALUES ('John', 'Doe', 'john@example.com')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
