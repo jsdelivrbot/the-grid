77bc67701a4116679ba2fdae9cd865d0092c3466
avong886 <alexvong8@gmail.com>
2016-01-24 13:14:16 -0800

    Update Alternative_fuels.js

--- directory/chem/Alternative_fuels.js
+++ directory/chem/Alternative_fuels.js
@@ -1,4 +1,7 @@
 var alternative_fuels= modern life was made by discovery and refinery of fossil fuels.  
+
+Needed: energy replenish able by the Sun
+
 Fossil fuels = organic matter.decay();
 Fossil_fuels=[];
 
@@ -33,3 +36,19 @@ Fossil fuels.push(['coal, gas, and oil for electrical power']);
 // most of hydrogen production relies on hydrocarbons in:
 //petroleum
 //coal
+
+Ethanol by fermentation. Of biomass. O2 atom ensures complete combustion., but partial oxygenation reduces Energon capacity
+
+Methane: renewable, from bacterial digestion of agricultural wastes. Environment::oxygen absent::. Formula// CH4. To increase rate of generation on a large scale, <sugar> added.
+**less useful than ethanol, as a gas, has low enthalpy density (enthalpy of combustion per Liter).
+Use:: capable wherever natural gas in used
+
+Biodiesel: alias for diesel fuel from algae or vegetable oil. Context('renewable organic sources');
+@includes: used oils, from fryers. 
+::More efficient than gasoline engines. 
+::high energy density (enthalpy of combustion per Liter)
+(<>)however, it is more viscous and may solidify at lower temperatures
+
+Substances contribute to greenhouse effect, produce co2 when burned. But plants can renew energy.
+The Sun replenishes the energy.
+
