f9cb34417bfa3f6b4cd0b3145c8f35b93a43d8fb
avong886 <alexvong8@gmail.com>
2016-02-21 21:55:17 -0800

    Update pages.js

--- Tron/pages.js
+++ Tron/pages.js
@@ -1,3 +1,6 @@
-//http://wikifoundryattachments.com/GAxBUGcHc0khLtOKwwB9Pw5340
+var tron1 = new Audio();
+tron1.src="http://wikifoundryattachments.com/GAxBUGcHc0khLtOKwwB9Pw5340"
 
-//http://wikifoundryattachments.com/XIeItiX6hM5d5Ra7dkUtVQ3081
+
+var img= new Audio();
+img.src="http://wikifoundryattachments.com/hMRSeDD9DlKKDyse8PL0Vw3784"
